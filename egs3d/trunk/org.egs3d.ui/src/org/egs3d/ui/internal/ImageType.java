20fd3598f733df99e66dac0be26719bd4a7b79ff
brachet <brachet@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-06 23:23:11 +0000

    Ajout des icones

--- egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/internal/ImageType.java
+++ egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/internal/ImageType.java
@@ -30,7 +30,10 @@ package org.egs3d.ui.internal;
  */
 public enum ImageType {
     OPENED_PROJECT("prj_obj.gif"), CLOSED_PROJECT("cprj_obj.gif"), FILE("file_obj.gif"), FOLDER( //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-            "fldr_obj.gif"); //$NON-NLS-1$
+            "fldr_obj.gif"), BRANCHGROUP("bgrp_obj.gif"), TRANSFORMGROUP("tgrp_obj.gif"),
+            TEXTURE("txtr_obj.gif"), MODELE("mdl_obj.gif"), LOCALE("lcl_obj.gif"),
+            SHAPE("shp_obj.gif"), LIGHT("lgt_obj.gif"), BACKGROUND("bgd_obj.gif"),
+            ; //$NON-NLS-1$
 
     private static final String PREFIX_PATH = "/icons/obj16/"; //$NON-NLS-1$
     private final String path;
