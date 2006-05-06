c9d91833e077b94c6430d964664ce818b815673a
romale <romale@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-06 17:52:25 +0000

    ajout des méthodes getScene()

--- egs3d/trunk/org.egs3d.core.resources/src/org/egs3d/core/resources/ResourcesPlugin.java
+++ egs3d/trunk/org.egs3d.core.resources/src/org/egs3d/core/resources/ResourcesPlugin.java
@@ -23,7 +23,13 @@
 package org.egs3d.core.resources;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Plugin;
 import org.egs3d.core.resources.internal.Scene;
 import org.egs3d.core.resources.internal.SceneReader;
@@ -36,6 +42,9 @@ import org.egs3d.core.resources.internal.SceneWriter;
  * @author romale
  */
 public class ResourcesPlugin extends Plugin {
+    public static final String PLUGIN_ID = "org.egs3d.core.resources";
+
+
     /**
      * Retourne une instance de l'interface {@link ISceneReader}.
      */
@@ -66,5 +75,54 @@ public class ResourcesPlugin extends Plugin {
      */
     public static IScene createScene() {
         return new Scene();
+    }
+
+
+    /**
+     * Retourne une scène contenue dans un fichier. Si le fichier vient d'être
+     * créé, une nouvelle scène sera initialisée. Si le fichier n'a pas
+     * l'extension d'une scène, la valeur <code>null</code> est retournée.
+     */
+    public static IScene getScene(IFile file) throws IOException, CoreException {
+        if (!SceneConstants.SCENE_FILE_EXTENSION.equals(file.getFileExtension())) {
+            return null;
+        }
+
+        IScene scene = null;
+        if (!file.exists()) {
+            final File tempFile = File.createTempFile("scene-", //$NON-NLS-1$
+                    "." + SceneConstants.SCENE_FILE_EXTENSION); //$NON-NLS-1$
+            tempFile.deleteOnExit();
+
+            // TODO optimiser le code : on écrit 2 fois sur le disque
+            createSceneWriter().write(tempFile, createScene(file.getProject()));
+            file.create(new FileInputStream(tempFile), true, null);
+            tempFile.delete();
+        }
+
+        scene = (IScene) file
+                .getSessionProperty(SceneConstants.SCENE_SESSION_RESOURCE_NAME);
+        if (scene == null) {
+            final File javaFile = file.getLocation().toFile();
+            scene = createSceneReader().read(javaFile);
+            scene.setProject(file.getProject());
+            file.setSessionProperty(SceneConstants.SCENE_SESSION_RESOURCE_NAME, scene);
+        }
+
+        return scene;
+    }
+
+
+    public static IScene getScene(Object obj) {
+        if (obj instanceof ISceneObject) {
+            return ((ISceneObject) obj).getScene();
+        }
+        if (obj instanceof IFile) {
+            try {
+                return getScene((IFile) obj);
+            } catch (Exception e) {
+            }
+        }
+        return null;
     }
 }
