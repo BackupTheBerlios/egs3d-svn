1311c39fa0a7cca994641f712a1b88e3d5c9a3c1
romale <romale@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-07 01:24:26 +0000

    implémentation de l'aperçu d'un modèle

--- egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/views/PreviewView.java
+++ egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/views/PreviewView.java
@@ -23,7 +23,20 @@
 package org.egs3d.ui.views;
 
 
+import java.awt.Color;
 import java.lang.ref.WeakReference;
+
+import javax.media.j3d.AmbientLight;
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.Bounds;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.DirectionalLight;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -41,9 +54,15 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.PageBook;
 import org.eclipse.ui.part.ViewPart;
+import org.egs3d.core.resources.IModel;
 import org.egs3d.core.resources.ITexture;
 import org.egs3d.ui.internal.Messages;
 import org.egs3d.ui.internal.SWTUtils;
+import org.egs3d.ui.util.SWTCanvas3D;
+
+import com.sun.j3d.utils.behaviors.keyboard.KeyNavigatorBehavior;
+import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseWheelZoom;
 
 
 /**
@@ -57,6 +76,7 @@ public class PreviewView extends ViewPart implements ISelectionListener {
     private PageBook pageBook;
     private ScrolledComposite imageComp;
     private CLabel imageLabel;
+    private SWTCanvas3D canvas3D;
     private WeakReference<Object> selectionRef;
 
 
@@ -71,6 +91,8 @@ public class PreviewView extends ViewPart implements ISelectionListener {
         imageComp = new ScrolledComposite(pageBook, SWT.H_SCROLL | SWT.V_SCROLL);
         imageLabel = new CLabel(imageComp, SWT.NONE);
         imageComp.setContent(imageLabel);
+
+        canvas3D = new SWTCanvas3D(pageBook, SWT.NONE);
 
         // l'élément sélectionné dans l'explorateur de scène est récupéré à
         // travers l'interface ISelectionListener
@@ -106,7 +128,7 @@ public class PreviewView extends ViewPart implements ISelectionListener {
 
 
     private void showTexture(ITexture texture) {
-        if (texture.getImage() == null) {
+        if (texture == null || texture.getImage() == null) {
             showNoPreview();
             return;
         }
@@ -120,6 +142,71 @@ public class PreviewView extends ViewPart implements ISelectionListener {
         imageLabel.layout();
 
         pageBook.showPage(imageComp);
+    }
+
+
+    private void showModel(IModel model) {
+        if (model == null || model.getBranchGroup() == null) {
+            showNoPreview();
+            return;
+        }
+
+        final Bounds bounds = new BoundingSphere(new Point3d(), 100000);
+        final BranchGroup root = new BranchGroup();
+        root.setCapability(BranchGroup.ALLOW_DETACH);
+
+        final Background bg = new Background(new Color3f(Color.WHITE));
+        bg.setApplicationBounds(bounds);
+        root.addChild(bg);
+
+        final BranchGroup sceneGraph = model.getBranchGroup();
+        final Transform3D scale3D = new Transform3D();
+        scale3D.setScale(0.7);
+        final TransformGroup scaleGroup = new TransformGroup(scale3D);
+        scaleGroup.addChild(sceneGraph);
+
+        final TransformGroup mouseRotateTG = new TransformGroup();
+        mouseRotateTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        final MouseRotate mouseRotate = new MouseRotate(mouseRotateTG);
+        mouseRotate.setSchedulingBounds(bounds);
+        mouseRotateTG.addChild(mouseRotate);
+        mouseRotateTG.addChild(scaleGroup);
+
+        final TransformGroup mouseZoomTG = new TransformGroup();
+        mouseZoomTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        final MouseWheelZoom mouseWheelZoom = new MouseWheelZoom(mouseZoomTG);
+        mouseWheelZoom.setSchedulingBounds(bounds);
+        mouseZoomTG.addChild(mouseWheelZoom);
+        mouseZoomTG.addChild(mouseRotateTG);
+
+        final TransformGroup keyNavigatorTG = new TransformGroup();
+        keyNavigatorTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        final KeyNavigatorBehavior keyNavigator = new KeyNavigatorBehavior(keyNavigatorTG);
+        keyNavigator.setSchedulingBounds(bounds);
+        keyNavigatorTG.addChild(keyNavigator);
+        keyNavigatorTG.addChild(mouseZoomTG);
+
+        final AmbientLight ambientLightNode = new AmbientLight(new Color3f(0.1f, 0.1f,
+                0.1f));
+        ambientLightNode.setInfluencingBounds(bounds);
+        root.addChild(ambientLightNode);
+
+        final Vector3f light1Direction = new Vector3f(1.0f, 1.0f, 1.0f);
+        final DirectionalLight light1 = new DirectionalLight(
+                new Color3f(1.0f, 1.0f, 0.9f), light1Direction);
+        light1.setInfluencingBounds(bounds);
+        root.addChild(light1);
+
+        final Vector3f light2Direction = new Vector3f(-1.0f, -1.0f, -1.0f);
+        final DirectionalLight light2 = new DirectionalLight(
+                new Color3f(1.0f, 1.0f, 0.9f), light2Direction);
+        light2.setInfluencingBounds(bounds);
+        root.addChild(light2);
+
+        root.addChild(keyNavigatorTG);
+        canvas3D.setSceneGraph(root);
+
+        pageBook.showPage(canvas3D);
     }
 
 
@@ -151,6 +238,8 @@ public class PreviewView extends ViewPart implements ISelectionListener {
 
         if (selectedObj instanceof ITexture) {
             showTexture((ITexture) selectedObj);
+        } else if (selectedObj instanceof IModel) {
+            showModel((IModel) selectedObj);
         } else {
             // pas d'aperçu pour l'élément sélectionné
             showNoPreview();
