e45103e527a670ca40b59a9f0aba0bca0f5dddc9
brachet <brachet@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-08 13:40:55 +0000

    Creation de la vue Code Generation

---
+++ egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/views/CodeGenerationView.java
@@ -0,0 +1,38 @@
+/*
+ * Créé le 8 mai 2006
+ *
+ * TODO Pour changer le modèle de ce fichier généré, allez à :
+ * Fenêtre - Préférences - Java - Style de code - Modèles de code
+ */
+package org.egs3d.ui.views;
+
+//import org.eclipse.jface.viewers.
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+ * Affichage du code java généré par EGS3D
+ * 
+ * @author brachet
+ */
+public class CodeGenerationView extends ViewPart {
+    
+	public CodeGenerationView() {
+		super();
+		// TODO Raccord de constructeur auto-généré
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Raccord de méthode auto-généré
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Raccord de méthode auto-généré
+
+	}
+
+}
