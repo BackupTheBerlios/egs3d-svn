54b144b534e6dbdd0db86163db8cd9e8dcb8c380
romale <romale@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-14 18:33:25 +0000

    implémentation de la génération de code

--- egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/views/CodeGenerationView.java
+++ egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/views/CodeGenerationView.java
@@ -1,47 +1,137 @@
-/*
- * Créé le 8 mai 2006
+/**
+ * $Id: DiagnosticView.java 86 2006-04-01 20:56:48Z romale $
  *
- * TODO Pour changer le modèle de ce fichier généré, allez à :
- * Fenêtre - Préférences - Java - Style de code - Modèles de code
+ * EGS3D
+ * http://egs3d.berlios.de
+ * Copyright (c) 2006 EGS3D team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 package org.egs3d.ui.views;
 
+
+import javax.media.j3d.Node;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.TextViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
+import org.egs3d.codegen.CodeGenPlugin;
+import org.egs3d.codegen.ICodeGenerator;
+import org.egs3d.core.StringUtils;
+import org.egs3d.core.resources.IScene;
+import org.egs3d.core.resources.ISceneObject;
+import org.egs3d.ui.internal.UIPlugin;
 
 
 /**
- * Affichage du code java généré par EGS3D
+ * Affichage du code Java généré par EGS3D.
  * 
  * @author brachet
+ * @author romale
  */
-public class CodeGenerationView extends ViewPart {
-	private TextViewer textViewer;
-    
-	@Override
-	public void createPartControl(Composite parent) {
-		textViewer = new TextViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
-		parent.setLayout(new FillLayout());
+public class CodeGenerationView extends ViewPart implements ISelectionListener {
+    private final Log log = LogFactory.getLog(getClass());
+    private TextViewer textViewer;
+    private Font font;
 
-		textViewer.setEditable(false);
-		textViewer.setDocument(new Document("Hello world!"));
-	}
 
-	@Override
-	public void setFocus() {
-		textViewer.getControl().setFocus();
-	}
+    @Override
+    public void createPartControl(Composite parent) {
+        textViewer = new TextViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+        parent.setLayout(new FillLayout());
 
-	@Override
-	public void dispose() {
-		if(textViewer != null) {
-			textViewer.getControl().dispose();
-			textViewer = null;
-		}
-		super.dispose();
-	}
+        font = new Font(Display.getCurrent(), "Courier New", 10, 0);
+        textViewer.getTextWidget().setFont(font);
+
+        textViewer.getTextWidget().setTabs(4);
+        textViewer.setEditable(false);
+        textViewer.setDocument(new Document(StringUtils.EMPTY_STRING));
+
+        getSite().getWorkbenchWindow().getSelectionService().addPostSelectionListener(
+                SceneExplorerView.VIEW_ID, this);
+    }
+
+
+    @Override
+    public void setFocus() {
+        textViewer.getControl().setFocus();
+    }
+
+
+    @Override
+    public void dispose() {
+        if (textViewer != null) {
+            getSite().getWorkbenchWindow().getSelectionService()
+                    .removePostSelectionListener(SceneExplorerView.VIEW_ID, this);
+
+            textViewer.getControl().dispose();
+            textViewer = null;
+        }
+        if (font != null) {
+            font.dispose();
+            font = null;
+        }
+        super.dispose();
+    }
+
+
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+        final IStructuredSelection sel = (IStructuredSelection) selection;
+        if (sel.isEmpty() || sel.getFirstElement() == null) {
+            return;
+        }
+
+        final Object selectedObj = sel.getFirstElement();
+        if (selectedObj instanceof Node) {
+            generateCode(((Node) selectedObj).cloneTree(true));
+        } else if (selectedObj instanceof IFile) {
+            final IScene scene = UIPlugin.getDefault().getSelectedScene();
+            if (scene != null) {
+                generateCode(scene);
+            }
+        } else if (selectedObj instanceof ISceneObject) {
+            final IScene scene = ((ISceneObject) selectedObj).getScene();
+            generateCode(scene);
+        }
+    }
+
+
+    private void generateCode(Object object) {
+        log.info("Génération du code pour l'élément : " + object);
+
+        final ICodeGenerator codeGen = CodeGenPlugin.createCodeGenerator();
+        final CharSequence code;
+        if (codeGen.isSupported(object)) {
+            code = codeGen.generate(object);
+        } else {
+            code = null;
+        }
+        textViewer.getDocument().set(
+                code == null ? StringUtils.EMPTY_STRING : code.toString());
+    }
 }
