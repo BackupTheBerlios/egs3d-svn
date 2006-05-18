1823d91fb051ea52b0249320cbe0225d08f4f28c
romale <romale@6cece7f3-3f0d-0410-a380-a7ee73e52ffe>
2006-05-18 23:25:20 +0000

    ajout d'une lumière

--- egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/actions/InsertLightAction.java
+++ egs3d/trunk/org.egs3d.ui/src/org/egs3d/ui/actions/InsertLightAction.java
@@ -6,8 +6,12 @@
  */
 package org.egs3d.ui.actions;
 
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.Node;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3f;
 
 import org.eclipse.ui.IWorkbenchWindow;
 
@@ -20,7 +24,12 @@ import org.eclipse.ui.IWorkbenchWindow;
 public class InsertLightAction extends AbstractInsertAction {
 	@Override
 	protected Node createNode(IWorkbenchWindow window) {
-		// TODO choisir le type de lumière
-		return new DirectionalLight();
+        // TODO choisir le type de lumière
+        final Vector3f lightDirection = new Vector3f(1.0f, 1.0f, 1.0f);
+        final DirectionalLight light = new DirectionalLight(new Color3f(1.0f, 1.0f,
+                0.9f), lightDirection);
+        light.setInfluencingBounds(new BoundingSphere(new Point3d(), Double.POSITIVE_INFINITY));
+
+        return light;
 	}
 }
